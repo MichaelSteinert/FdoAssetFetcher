b8f4d0bdc8c551f7779ed535f2034d31a48a02e8
Henrik tom Wörden <h.tomwoerden@indiscale.com>
2024-06-08 20:47:37 +0200

    wip

--- app/src/main/java/fdoassetfetcher/MetadataRepository.java
+++ app/src/main/java/fdoassetfetcher/MetadataRepository.java
@@ -1,20 +1,37 @@
 package fdoassetfetcher;
 
-import com.indiscale.fdo.manager.DefaultMetadat;
+import com.indiscale.fdo.manager.DefaultData;
+import com.indiscale.fdo.manager.DefaultFdo;
+import com.indiscale.fdo.manager.DefaultMetadata;
 import com.indiscale.fdo.manager.api.*;
-import com.indiscale.fdo.manager.mock.MockManager;
+import com.indiscale.fdo.manager.doip.DoipRepository;
+import com.indiscale.fdo.manager.util.Util;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class MetadataRepository {
     public static void addDatasetToRepository(EdcMetadata edcMetadata, String profileName) throws Exception {
-        try (Manager manager = new MockManager()) {
-            FdoProfile profile = manager.getProfileRegistry().getProfile(profileName);
-            RepositoryConnection repository = manager.getRepositoryRegistry().createRepositoryConnection("mock-repo-1");
+
+        String linkaheadJson = System.getProperty("test.linkahead.json", ".test.linkahead.json");
+
+        RepositoryConfig config = null;
+        try {
+          config = Util.jsonToRepositoryConfig(new File(linkaheadJson));
+        } catch (FileNotFoundException e) {
+          System.err.println(
+              "WARNING: Cannot setup connection to LinkAhead since the config file `"
+                  + linkaheadJson
+                  + "` does not exist.");
+        }
+        try (DoipRepository repository = new DoipRepository(config)) {
+			FdoProfile profile = FdoProfile.GENERIC_FDO;
             InputStream mdInputStream = new ByteArrayInputStream(edcMetadata.toString().getBytes());
-            Metadata metadata = new DefaultMetadat(mdInputStream);
-            FDO fdo = manager.createFDO(profile, repository, null, metadata);
+            Metadata metadata = new DefaultMetadata(mdInputStream);
+            Data data = new DefaultData(new ByteArrayInputStream("https://jsonplaceholder.typicode.com/todos".getBytes())); //TODO replace by data URL
+			FDO fdo = repository.createFDO(new DefaultFdo(null, profile, data, metadata));
             System.out.println("Created FDO with PID: " + fdo.getPID());
         }
     }
